169c157611055615d7e1de1416cf7836b8586872
localhost <greichenbach@andover.edu>
2017-12-26 11:17:34 +0800

    Adding unit tests and GlobSearch retrieval method

---
+++ adlframework/retrievals/GlobSearch.py
@@ -0,0 +1,32 @@
+from .retrieval import Retrieval
+import os
+from glob2 import iglob
+
+class GlobSearch(Retrieval):
+	'''
+	Uses the glob module to find all files recursively matching description.
+	Does not currently find labels
+	'''
+	_return_type = 'file path'
+	def __init__(self, superdir, file_regex):
+		super(GlobSearch, self).__init__()
+		self.file_regex = file_regex
+		self.superdir = os.path.abspath(superdir)+'/'
+
+	def get_data(self, id_):
+		'''
+		Returns a file path.
+		'''
+		return id_
+
+	def get_label(self, id_):
+		'''
+		Returns a file path.
+		'''
+		return None
+
+	def list(self):
+		'''
+		Returns a list of unique string identifiers.
+		'''
+		return iglob(self.superdir+'**/'+self.file_regex)
