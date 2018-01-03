535f959c3ff5e96a97bc876323d18856d42f5fa0
localhost <greichenbach@andover.edu>
2018-01-03 08:26:53 -0500

    Adding augmentations to boat_disc problem

--- examples/BoatIceburgDiscriminator/boat_disc_example.py
+++ examples/BoatIceburgDiscriminator/boat_disc_example.py
@@ -1,6 +1,7 @@
 from functools import partial
 import pdb
 import os
+from imgaug import augmenters as iaa
 ### Data
 from adlframework.retrievals.JsonFile import JsonFile
 from adlframework.datasource import DataSource
@@ -8,11 +9,12 @@ from adlframework.dataentity.image_de import ImageFileDataEntity
 ### Model
 from adlframework.nets.image_nets import medium_model
 from keras.optimizers import Adadelta
-from keras.losses import MAE
+from keras.losses import KLD
 from adlframework.experiment import SimpleExperiment
 ### Controllers
 from adlframework.processors.general_processors import reshape, make_categorical
 from adlframework.filters.general_filters import accept_label, ignore_label
+from adlframework.augmentations.image_augmentations import imgaug_augment
 ### Callbacks
 from keras.callbacks import ModelCheckpoint
 from adlframework.callbacks.image_callbacks import SaveValImages
@@ -31,8 +33,18 @@ val_retrieval = JsonFile(fp=abs_path+'val.json',
 net = medium_model(input_shape=(90, 125, 1),
 					target_shape=(1))
 
+### Augmentation
+seq = iaa.Sequential(
+    [
+        iaa.Fliplr(0.5), # horizontally flip 50% of all images
+        iaa.Flipud(0.2), # vertically flip 20% of all images
+    ],
+    random_order=True
+)
+
 ## Controllers
-controllers = [partial(reshape, shape=(90, 125, 1))]
+controllers = [partial(reshape, shape=(90, 125, 1)),
+				partial(imgaug_augment, sequence=seq)]
 
 #### TRAINING DATASOURCE
 iceberg_trainds = DataSource(train_retrieval, ImageFileDataEntity,
@@ -59,7 +71,7 @@ callbacks = [ModelCheckpoint('weights.{epoch:02d}-{val_loss:.2f}.hdf5')]
 #### Define experiment
 exp = SimpleExperiment(train_datasource=train_ds,
 						validation_datasource=val_ds,
-						loss=MAE,
+						loss=KLD,
 						optimizer=Adadelta(),
 						network = net,
 						callbacks=callbacks)
