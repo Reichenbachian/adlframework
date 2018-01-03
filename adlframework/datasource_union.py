239ba6e5addea3d537fdfbfa42e70e0cb054a0cb
localhost <greichenbach@andover.edu>
2018-01-03 08:14:34 -0500

    Removing pdb trace from datasource_union

--- adlframework/datasource_union.py
+++ adlframework/datasource_union.py
@@ -6,6 +6,7 @@ union = ds1 + different_union
 import logging
 import math
 import pdb
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +22,7 @@ class DataSourceUnion():
 
 	def next(self, percentages=None):
 		'''
-		Returns a batch taken from the datasources of the conglomerate.
+		Returns a batch taken from the datasources of the union.
 
 		If percentages is defined, each batch is composed of the percent of each
 		datasource as is defined by percentages.
@@ -43,6 +44,5 @@ class DataSourceUnion():
 				batch = ds.next(bs)
 				batch_X.extend(batch[0])
 				batch_y.extend(batch[1])
-				pdb.set_trace()
-			return batch_X, batch_y
+			return np.array(batch_X), np.array(batch_y)
 
