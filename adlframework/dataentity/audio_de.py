1e4bfaf01bf3ef64c51e606fb06a9d0f5af6076b
localhost <greichenbach@andover.edu>
2018-01-09 21:28:14 -0500

    Changing audio file class to audio segment class

--- adlframework/dataentity/audio_de.py
+++ adlframework/dataentity/audio_de.py
@@ -10,65 +10,46 @@ logger = logging.getLogger(__name__)
 
 
 class AudioFileDataEntity(DataEntity):
+    '''
+    This represents a short audio segment. One that can be clipped on both
+    ends for training without losing a significant amount of data.
+
+    length - length in seconds of audio file
+    fs     - sampling rate of audio file
+    '''
     # original audio file length
     length   = attr.ib(validator=attr.validators.instance_of(float))
     # Sampling rate
     fs   = attr.ib(validator=attr.validators.instance_of(int))
     
 
-    def __init__(self, unique_id, retrieval, window_length, pad="zeros"):
+    def __init__(self, unique_id, retrieval, pad="zeros"):
         '''
         Window length is in seconds.
         '''
         self.unique_id = unique_id
         self.retrieval = retrieval
-        self.window_length = window_length
         self.pad = pad
         self.labels = pd.read_csv(retrieval.get_label(unique_id))
-
-        #### Make sure timestamp column is present. 
-        if self.TIMESTAMP_KEY not in self.labels.columns:
-            if len(self.labels) == 1:
-                self.labels[0] = 0
-            else:
-                raise Exception('Multiple entries in label without timestamps')
-
-        self.labels = self.labels.sort_values(self.TIMESTAMP_KEY)
         
     def _read_file(self):
-        """
+        '''
         Reads the raw data from the retrieval virtual or real file.
         Don't call these
-        """
+        '''
         f = self.retrieval.get_data(self.unique_id)
-        # Load wav files
-        self.fs, audio = wav.read(f)
+        self.fs, self.data = wav.read(f)
         self.length = len(audio)/float(self.fs)
         return audio
 
     def get_sample(self):
         '''
-            Given a numpy array of returned sample, it returns a sample.
+        Given a numpy array of returned sample, it returns a sample.
         '''
-        raw_arr = self.get_data()
+        if self.labels is None: # Read labels into memory.
+            self.labels = self.retrieval.get_label(self.unique_id)
+        if self.data is None: # Read data into memory.
+            self.get_data()
 
-        #### Data
-        label = self.labels.iloc[int(random.random()*len(self.labels))]
-        start_time = label[self.TIMESTAMP_KEY]
-        start_index = int(start_time*self.fs)
-        raw_arr = raw_arr[start_index:start_index+self.window_length*self.fs]
-
-        ## the below lines are for selecting a subsegment if the length is too long
-        if raw_arr.shape[0] < int(self.window_length*self.fs):
-            #### Data
-            ## the below lines are for zero padding if audio_len < win_length
-            pad = None
-            if self.pad.lower() == 'zeros':
-                pad = np.zeros(int(self.window_length*self.fs))
-            elif self.pad.lower() == 'noise':
-                pad = np.random.noise(int(self.window_length*self.fs))
-            pad[:len(raw_arr)] = raw_arr
-            raw_arr = pad
-
-        return raw_arr, label
+        return self.data, self.labels
         
