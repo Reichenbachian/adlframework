b258639bf735185e48fb76c30f0106f3c62e0670
localhost <greichenbach@andover.edu>
2018-01-12 15:55:46 -0500

    Adding warning suppression options

--- adlframework/processors/midi_processors.py
+++ adlframework/processors/midi_processors.py
@@ -4,12 +4,12 @@ import numpy as np
 #### PROCESSORS   ######
 ########################
 
-def midi_to_np(sample, targets=[], reverse=None):
+def midi_to_np(sample, targets=[], reverse=None, suppress_warnings=False):
 	'''
 	Converts a music21 midi stream object to a numpy array.
 	'''
 	stream, label = sample
-	return stream.notes(), label
+	return stream.notes(suppress_warnings=suppress_warnings), label
 
 
 ###############################
