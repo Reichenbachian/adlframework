cd537f2198f91cedf3369f68a884a02dad5a53e2
localhost <greichenbach@andover.edu>
2018-01-23 16:26:41 -0500

    inserting print in async

--- adlframework/datasource.py
+++ adlframework/datasource.py
@@ -88,7 +88,8 @@ class DataSource():
 		assert len(self._entities) > 0, "Cannot initialize an empty data source"
 
 	def async_fill_queue(self):
-		while not self.sample_queue.full(): # Worst case: Very fast processor, very large queue. Then slow first load.
+		while not self.sample_queue.full():
+			print(self.list_pointer)
 			self.entity_queue.put(self._entities[self.list_pointer])
 			self.list_pointer += 1
 			if self.list_pointer >= len(self._entities):
