d02a08c24d618c7440dcb10f712e0d43ad131b48
localhost <greichenbach@andover.edu>
2018-01-30 11:03:15 -0500

    Updated audio dataentity. Still needs testing.

--- adlframework/dataentity/audio_de.py
+++ adlframework/dataentity/audio_de.py
@@ -70,6 +70,9 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
     This represents a longer audio segment. One that has many internal
     labels delimited by timestamps.
 
+    Arguments
+    window_size: The size of the window in 
+
     Preconditions
     Labels contain a 'timestamp' column!
 
@@ -89,10 +92,13 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
     length       - length in seconds of audio file
     fs           - sampling rate of audio file
     window_size  - sampling rate of audio file
+    self.data    - a 1-d representation of audio.
 
     '''
     # To-Do: Implement!
-    def __init__(self, window_size=None, padding='zeros', sampling_method='linear_interpolation',
+    def __init__(self, window_size=None, padding='zeros',
+                    timestamp_units='seconds', window_units='seconds',
+                    sampling_method='discrete_sample',
                     **kwargs):
         '''
         window_size: In seconds. If it is None, variable lengths will be returned.
@@ -105,10 +111,16 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
             - 'discrete': Don't cross over between labels, and keep labels untouched.
         '''
         DataEntity.__init__(self, **kwargs)
+        assert window_size == 'seconds' or window_size == 'frames', 'window_size may only be seconds or frames'
+        assert timestamp_units == 'seconds' or window_units == 'frames', 'timestamp_units may only be seconds or frames'
+        assert not (window_size == None and sampling_method == 'linear_interpolation'), 'If it is linear interpolating, a window_size must be provided.'
+        assert sampling_method == 'discrete_sample' or sampling_method == 'interpolate_sample', 'Only discrete_sample or interpolate_sample are implemented'
         self.padding = padding
         self.indexed = False
         self.sampling_method = sampling_method
         self.window_size = window_size
+        self.timestamp_units = timestamp_units
+        self.window_units = window_units
 
     def index(self):
         '''
@@ -118,13 +130,21 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
         self.labels = self.labels.sort_values(by='timestamp')
         self.indexed = True
 
-        ### To-Do: remove those labels that are outside the range of start_time+window_size*fs
+        ### Remove labels that are outside the range of start_time+window_size*fs
+        if self.window_size:
+            max_time = len(self.data) - self.window_size
+            max_time = max_time if self.timestamp_units == 'frames' else max_time/self.fs
+            self.labels = self.labels[(self.labels['timestamp'] < max_time) & (self.labels['timestamp'] > 0)]
+
 
     def discrete_sample(self):
         '''
         Samples without crossing over between labels.
         Meaning the dataframe below will only ever return
-        two samples. Sample_1=(2.3, 2.3+window_size*fs) and Sample_2=(4.5, 4.5+window_size*fs)
+        two samples. Sample_1=(2.3, 2.3+window_size) and Sample_2=(4.5, 4.5+window_size)
+
+        If window_size is None, the length of the sample is between
+        the timestamp sampled and the next.
 
         | timestamp | metric_1 | metric_1 |
         -----------------------------------
@@ -135,15 +155,37 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
         |    5.2    |    5.4   |   3.1    |
         -----------------------------------
         '''
-        # Choose a label, except not the last one, as we can't sample off the end of the file.
-        sampled_label = self.labels.iloc[np.random.choice(len(self.labels))] 
+        # Choose a start time
+        label_i = np.random.choice(len(self.labels))
+        sampled_label = self.labels.iloc[label_i] 
+        start_time = sampled_label['timestamp']
+        end_time = None
+        if timestamp_units == 'seconds': ### Convert to frame
+            start_time *= self.fs
+
+        # Create an end time
+        end_time = None
+        if window_size == None:
+            if label_i == len(self.labels)-1:
+                end_time = len(self.data)
+            else:
+                end_time = self.labels.iloc[label_i+1]
+                if timestamp_units == 'seconds': ### Convert to frame
+                    end_time *= self.fs
+        else:
+            end_time = start_time + self.window_size
+
+        # Create and return sample
+        sampled_data = self.data[start_time:end_time]
+        return sampled_data, sampled_label
 
 
     def interpolate_sample(self):
         '''
         Samples continuously. Crossing over labels if necessary.
         For instance, one sample might be (4.4, 4.4+window_size*fs)
-        and the label for metric_1 will become the weighted sum.
+        and the label for metric_1 will become the weighted sum of the
+        two closest labels to the start and end time.
 
         | timestamp | metric_1 | metric_1 |
         -----------------------------------
@@ -154,6 +196,19 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
         |    5.2    |    5.4   |   3.1    |
         -----------------------------------
         '''
+        # Get start and end time and data
+        start_frame = int(np.random.random()*len(self.data))
+        end_frame = start_frame + self.window_size
+        sampled_data = self.data[start_time:end_time]
+
+        # Create label
+        start_timestamp = start_frame if self.timestamp_units == 'frames' else start_frame/self.fs
+        end_timestamp = end_frame if self.timestamp_units == 'frames' else end_frame/self.fs
+        start_label_i = (self.labels['timestamp']-start_timestamp).abs().argsort()[0]
+        end_label_i = (self.labels['timestamp']-end_timestamp).abs().argsort()[0]
+        sample_label = self.labels.ix[[start_label_i, end_label_i]].mean()
+
+        return sampled_data, sample_label
         
     def get_sample(self):
         '''
@@ -161,10 +216,13 @@ class AudioRecordingDataEntity(AudioFileDataEntity):
         '''
         if self.labels is None: # Read labels into memory.
             self.labels = self.retrieval.get_label(self.unique_id)
-        if self.indexed:
-            self.index()
         if self.data is None: # Read data into memory.
             self.get_data()
+            #### Convert second units to frame size
+            if self.window_units == 'seconds':
+                self.window_size *= self.fs
+        if self.indexed:
+            self.index()
 
         sampled_data = None
         sampled_label = None
