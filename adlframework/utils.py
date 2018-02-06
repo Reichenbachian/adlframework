dcd43cbdd7a91ab7037ab6e93843301637238edd
localhost <greichenbach@andover.edu>
2018-02-06 10:28:18 -0500

    Making log slightly better

--- adlframework/utils.py
+++ adlframework/utils.py
@@ -2,13 +2,14 @@ from logging.handlers import TimedRotatingFileHandler
 import logging
 
 logger = logging.getLogger("Logger_1")
+LOG_NAME = 'adlframework.log'
 
 def get_logger():
 	'''
 	Makes only one logger for the whole project.
 	'''
 	global logger
-	handler = TimedRotatingFileHandler('log')
+	handler = TimedRotatingFileHandler(LOG_NAME)
 	logger.addHandler(handler)
 	return logger
 
