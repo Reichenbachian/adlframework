4094cb520c81f67563c86bac7a9ee90736d8a4d7
localhost <greichenbach@andover.edu>
2018-02-15 14:54:32 -0500

    Changing verification code for ipython

--- adlframework/utils.py
+++ adlframework/utils.py
@@ -19,10 +19,6 @@ def in_ipynb():
 	Checks whether the code is currently being executed in a python notebook.
 	'''
 	try:
-		cfg = get_ipython().config 
-		if cfg['IPKernelApp']['parent_appname'] == 'ipython-notebook':
-			return True
-		else:
-			return False
+		cfg = get_ipython().config
 	except NameError:
 		return False
