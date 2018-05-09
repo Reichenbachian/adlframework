ac8ec7e8a5643dde4225968bb36659a13549815b
localhost <greichenbach@andover.edu>
2018-05-08 20:26:18 -0400

    Changing to filepath rather than csv

--- adlframework/retrievals/BlobLocalCache.py
+++ adlframework/retrievals/BlobLocalCache.py
@@ -43,7 +43,7 @@ class BlobLocalCache(Retrieval):
 		'''
 		Returns a file path.
 		'''
-		return pd.read_csv(self.label_dir+id_[:id_.rfind('.')]+'.csv')
+		return self.label_dir+id_[:id_.rfind('.')]+'.csv'
 
 	def list(self):
 		'''
